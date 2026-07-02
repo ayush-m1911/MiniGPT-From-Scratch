1a8158aa7243ccade55a6a252f6db8a790b5f5d7
ayush-m1911 <ayushmetkar3@gmail.com>
2026-07-02 23:05:43 +0530

    Implemented Feed Forward Neural Network

---
+++ models/feed_forward.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=None, dropout=0.1):
+        
+        super().__init__()
+        
+        if d_ff is None:
+            d_ff = 4*d_model
+        self.net = nn.Sequential(
+            nn.Linear(d_model,d_ff),
+            nn.GELU(),
+            nn.Linear(d_ff,d_model),
+            nn.Dropout(dropout)
+        )
+    
+    def forward(self,x):
+        return self.net(x)
